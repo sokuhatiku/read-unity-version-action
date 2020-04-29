a4b1f1ab65ecb880d99f97db9c36d781e9e409d0
Sokuhatiku <sokuhatiku@gmail.com>
2020-04-30 00:09:36 +0900

    テストを追加

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -2,12 +2,70 @@ import * as process from 'process'
 import * as cp from 'child_process'
 import * as path from 'path'
 
-// shows how the runner will run a javascript action with env / stdout protocol
-test('test runs', () => {
-  process.env['GITHUB_WORKSPACE'] = '__tests__/UnityProject_5.6.1f1'
+test('with projectPath input(5.6.1f1)', () => {
+  const customEnv:NodeJS.ProcessEnv = Object.assign(
+    { INPUT_PROJECTPATH:'__tests__/UnityProject_5.6.1f1' },
+    process.env
+  )
+  expect(execWithCustomEnv(customEnv))
+    .toContain('::set-output name=editorVersion,::5.6.1f1')
+})
+
+test('with projectPath input(2019.3.8f1)', () => {
+  const customEnv:NodeJS.ProcessEnv = Object.assign(
+    { INPUT_PROJECTPATH:'__tests__/UnityProject_2019.3.8f1' },
+    process.env
+  )
+  expect(execWithCustomEnv(customEnv))
+    .toContain('::set-output name=editorVersion,::2019.3.8f1')
+})
+
+test('without projectPath input(5.6.1f1)', () => {
+  const customEnv:NodeJS.ProcessEnv = Object.assign(
+    { GITHUB_WORKSPACE:'__tests__/UnityProject_5.6.1f1' },
+    process.env
+  )
+  expect(execWithCustomEnv(customEnv))
+    .toContain('::set-output name=editorVersion,::5.6.1f1')
+})
+
+test('without projectPath input(2019.3.8f1)', () => {
+  const customEnv:NodeJS.ProcessEnv = Object.assign(
+    { GITHUB_WORKSPACE:'__tests__/UnityProject_2019.3.8f1' },
+    process.env
+  )
+  expect(execWithCustomEnv(customEnv))
+    .toContain('::set-output name=editorVersion,::2019.3.8f1')
+})
+
+test('without anything', () => {
+  const customEnv:NodeJS.ProcessEnv = process.env
+  expect(() => execWithCustomEnv(customEnv))
+  .toThrowError()
+})
+
+test('with invalid directory', () => {
+  const customEnv:NodeJS.ProcessEnv = Object.assign(
+    { INPUT_PROJECTPATH:'__tests__/UnityProject_NotExistsProject' },
+    process.env
+  )
+  expect(() => execWithCustomEnv(customEnv))
+  .toThrowError()
+})
+
+test('with invalid format', () => {
+  const customEnv:NodeJS.ProcessEnv = Object.assign(
+    { INPUT_PROJECTPATH:'__tests__/UnityProject_InvalidVersionFormat' },
+    process.env
+  )
+  expect(() => execWithCustomEnv(customEnv))
+  .toThrowError()
+})
+
+function execWithCustomEnv(customEnv:NodeJS.ProcessEnv):string{
   const ip = path.join(__dirname, '..', 'lib', 'main.js')
   const options: cp.ExecSyncOptions = {
-    env: process.env
+    env: Object.assign(customEnv, process.env)
   }
-  console.log(cp.execSync(`node ${ip}`, options).toString())
-})
+  return cp.execSync(`node ${ip}`, options).toString()
+}
