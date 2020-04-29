9b60ab8010cfaa9ea90ebbffe58b2ac4088b70a2
Sokuhatiku <sokuhatiku@gmail.com>
2020-04-30 00:28:52 +0900

    不正なプロジェクトのフォーマットを弾くよう変更

--- src/main.ts
+++ src/main.ts
@@ -13,9 +13,18 @@ async function run(): Promise<void> {
     console.log(`project path is "${projectPath}"`);
 
     const versionFilePath =  path.join(projectPath, 'ProjectSettings/ProjectVersion.txt');
+    if(!fs.existsSync(versionFilePath))
+    {
+      throw new Error('The version file not exists. Project is not exists or invalid format.')
+    }
     const contents = fs.readFileSync(versionFilePath).toString();
     const parsed = yaml.safeLoad(contents);
     const editorVersion = parsed.m_EditorVersion as string;
+    const unityVersionPattern:RegExp = /^\d+\.\d+\.\d+f\d+$/m;
+    if(!unityVersionPattern.test(editorVersion))
+    {
+      throw new Error('Read version failed. Project is not exists or invalid format.')
+    }
     console.log(`project version is "${editorVersion}"`);
 
     core.setOutput('editorVersion', editorVersion);
