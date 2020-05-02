96856c24eaaf5f1ef90ddbde412239f37e53b9b2
Sokuhatiku <sokuhatiku@gmail.com>
2020-05-02 20:09:53 +0900

    パース後も同じ文字列になるかどうかのテストを追加

--- __tests__/unityVersion.test.ts
+++ __tests__/unityVersion.test.ts
@@ -1,22 +1,22 @@
 import {UnityVersion} from '../src/unityVersion'
 
 test('Unity 5 Versioning', () => {
-  UnityVersion.Parse('5.0.0f4')
-  UnityVersion.Parse('5.6.7f1')
+  expect(UnityVersion.Parse('5.0.0f4').toString()).toBe('5.0.0f4')
+  expect(UnityVersion.Parse('5.6.7f1').toString()).toBe('5.6.7f1')
 })
 
 test('Unity 2017 or lator Versioning', () => {
-  UnityVersion.Parse('2017.1.0f3')
-  UnityVersion.Parse('2017.4.39f1')
-  UnityVersion.Parse('2018.1.0f2')
-  UnityVersion.Parse('2018.4.22f1')
-  UnityVersion.Parse('2019.1.0f2')
-  UnityVersion.Parse('2019.3.12f1')
+  expect(UnityVersion.Parse('2017.1.0f3').toString()).toBe('2017.1.0f3')
+  expect(UnityVersion.Parse('2017.4.39f1').toString()).toBe('2017.4.39f1')
+  expect(UnityVersion.Parse('2018.1.0f2').toString()).toBe('2018.1.0f2')
+  expect(UnityVersion.Parse('2018.4.22f1').toString()).toBe('2018.4.22f1')
+  expect(UnityVersion.Parse('2019.1.0f2').toString()).toBe('2019.1.0f2')
+  expect(UnityVersion.Parse('2019.3.12f1').toString()).toBe('2019.3.12f1')
 })
 
 test('Beta and alpha releases', () => {
-  UnityVersion.Parse('2020.1.0b7')
-  UnityVersion.Parse('2020.2.0a9')
+  expect(UnityVersion.Parse('2020.1.0b7').toString()).toBe('2020.1.0b7')
+  expect(UnityVersion.Parse('2020.2.0a9').toString()).toBe('2020.2.0a9')
 })
 
 test('Unity 4 or earyer releases are not supported', () => {
